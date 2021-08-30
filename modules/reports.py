e607024f8d5e5cb48890ab20127c32573f0005e9
kelsen <kelsensantos@gmail.com>
2021-08-29 22:41:57 -0400

    melhoramentos em relatorio

--- modules/reports.py
+++ modules/reports.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datapane as dp
+import matplotlib.pyplot as plt
 from decouple import config
 from modules.database import Database
 
@@ -36,7 +37,12 @@ class Relatorios:
         x = locais['local'].value_counts().to_frame('Total')
         y = round((locais['local'].value_counts(normalize=True).to_frame('%') * 100), 1)
         locais_resumo = pd.concat([x, y], axis=1)
-        locais_plot = locais_resumo.T.plot(kind='bar', figsize=(10, 5), layout=(3, 1), rot=0, fontsize=14)
+
+        locais_plot1 = locais_resumo[['Total']].T.plot(kind='bar', figsize=(10, 5), rot=0, fontsize=14)
+        locais_plot2 = plt.figure()
+        locais_plot2 = locais_resumo['%'].plot(
+          kind='pie', figsize=(10, 5), fontsize=14, ylabel='', labels=None, legend=True, autopct='%1.1f%%'
+        )
 
         # criação do relatório
         report = dp.Report(
@@ -44,12 +50,16 @@ class Relatorios:
 
             dp.Group(
                 dp.Select(blocks=[
-                    dp.Plot(locais_plot, label='Gráfico'),
+                    dp.Plot(locais_plot1, label='Gráfico', caption='Quantidade por localização'),
+                    dp.Plot(locais_plot2, label='Pizza %', caption='Quantidade percentual'),
                     dp.DataTable(locais_resumo, label='Tabela', caption='Resumo de jogos por localização')
                 ]),
                 dp.DataTable(locais, caption="Jogos por localização"),
                 columns=2
-            )
+            ),
         )
 
         return report
+
+
+Datapane.atualizar_relatorio()
